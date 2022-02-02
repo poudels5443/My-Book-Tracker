70adea8a62c6527841f68d3417b64324b74023e8
Safal Poudel <safalpoudel0223@gmail.com>
2022-02-02 15:15:04 -0600

    Initial Setup For Data Loader

---
+++ my-book-tracker-data-loader/src/main/java/com/mybooktracker/mybooktrackerdataloader/MyBookTrackerDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.mybooktracker.mybooktrackerdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookTrackerDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBookTrackerDataLoaderApplication.class, args);
+	}
+
+}
