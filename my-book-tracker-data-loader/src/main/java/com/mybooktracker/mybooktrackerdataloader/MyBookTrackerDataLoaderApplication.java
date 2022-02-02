74a14d6819ba1ad55e71b298dd1884c991081df5
Safal Poudel <safalpoudel0223@gmail.com>
2022-02-02 16:24:00 -0600

    Successfully connected to DataStax's Cassandra Instance

--- my-book-tracker-data-loader/src/main/java/com/mybooktracker/mybooktrackerdataloader/MyBookTrackerDataLoaderApplication.java
+++ my-book-tracker-data-loader/src/main/java/com/mybooktracker/mybooktrackerdataloader/MyBookTrackerDataLoaderApplication.java
@@ -1,13 +1,27 @@
 package com.mybooktracker.mybooktrackerdataloader;
 
+import java.nio.file.Path;
+
+import com.mybooktracker.mybooktrackerdataloader.connection.DataStaxAstraProperties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class MyBookTrackerDataLoaderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyBookTrackerDataLoaderApplication.class, args);
 	}
+
+	@Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
 
 }
