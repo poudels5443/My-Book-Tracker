15134d4204c3ee4cfae72e04d51aa66a0a2c2aa1
Safal Poudel <safalpoudel0223@gmail.com>
2022-02-03 12:04:25 -0600

    Successfully loaded author_by_id and book_by_id tables in cassandra instance

--- my-book-tracker-data-loader/src/main/java/com/mybooktracker/mybooktrackerdataloader/MyBookTrackerDataLoaderApplication.java
+++ my-book-tracker-data-loader/src/main/java/com/mybooktracker/mybooktrackerdataloader/MyBookTrackerDataLoaderApplication.java
@@ -1,13 +1,27 @@
 package com.mybooktracker.mybooktrackerdataloader;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
 
 import com.mybooktracker.mybooktrackerdataloader.author.Author;
 import com.mybooktracker.mybooktrackerdataloader.author.AuthorRepository;
+import com.mybooktracker.mybooktrackerdataloader.book.Book;
+import com.mybooktracker.mybooktrackerdataloader.book.BookRepository;
 import com.mybooktracker.mybooktrackerdataloader.connection.DataStaxAstraProperties;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +29,8 @@ import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomi
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
 
 
 
@@ -26,6 +42,12 @@ public class MyBookTrackerDataLoaderApplication {
     @Autowired 
     AuthorRepository authorRepository;
 
+    @Autowired 
+    BookRepository bookRepository;
+
+    private String authorDumpLocation = "src/main/resources/authors.txt";
+    private String worksDumpLocation = "src/main/resources/works.txt";
+
 
     
 	public static void main(String[] args) {
@@ -33,18 +55,112 @@ public class MyBookTrackerDataLoaderApplication {
 	}
 
 
-    
+    private void initAuthors(){
+        Path path = Paths.get(authorDumpLocation);
+        try(Stream<String> lines = Files.lines(path)){
+            lines.forEach(line ->{
+                String jsonString = line.substring(line.indexOf("{"));
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonString);
+                    Author author = new Author();
+                    author.setName(jsonObject.optString("name"));
+                    author.setPersonalName(jsonObject.optString("personal_name"));
+                    author.setId(jsonObject.optString("key").replace("/authors/", ""));
+                    System.out.println("Name" + author.getName() + "..");
+                    authorRepository.save(author);
+
+                    
+                    
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            );
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void initWorks(){
+        Path path = Paths.get(worksDumpLocation);
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS");
+        try(Stream<String> lines = Files.lines(path)){
+            lines.forEach(line ->{
+                String jsonString = line.substring(line.indexOf("{"));
+                try {
+                    JSONObject jsonObject = new JSONObject(jsonString);
+                    Book book = new Book();
+                    book.setId(jsonObject.getString("key").replace("/works/", ""));
+
+                    book.setName(jsonObject.optString("title"));
+
+                    JSONObject descriptionObj = jsonObject.optJSONObject("description");
+                    if(descriptionObj != null){
+                        book.setDescription(descriptionObj.optString("value"));
+                    }
+                    
+                    JSONObject publishedObj = jsonObject.optJSONObject("created");
+                    if(publishedObj != null){
+                        String dateStr = publishedObj.getString("value");
+                        book.setPublishedDate(LocalDate.parse(dateStr, dateFormat));
+                    }
+
+                    JSONArray coversJSONArr = jsonObject.optJSONArray("covers");
+                    if(coversJSONArr != null){
+                        List<String> coverIds = new ArrayList<>();
+                        for(int i=0; i<coversJSONArr.length(); i++){
+                            coverIds.add(coversJSONArr.getString(i));
+                        }
+                        book.setCoverIds(coverIds);
+                    }
+
+                    JSONArray authorsJSONArr = jsonObject.optJSONArray("authors");
+                    if(authorsJSONArr != null){
+                        List<String> authorIds = new ArrayList<>();
+                        for(int i=0; i< authorsJSONArr.length(); i++){
+                            String authorId = authorsJSONArr.getJSONObject(i).getJSONObject("author").getString("key").replace("/authors/", "");
+                            authorIds.add(authorId);
+                        }
+                        book.setAuthorId(authorIds);
+                        List<String> authorNames = authorIds.stream().map(id -> authorRepository.findById(id))
+                        .map(optionalAuthor ->{
+                            if (!optionalAuthor.isPresent()) return "Unknown Author";
+                            return optionalAuthor.get().getName();
+                        }).collect(Collectors.toList());
+
+                        book.setAuthorNames(authorNames);
+
+                        
+                    }
+
+                    System.out.println("Saving book " + book.getName() + "...");
+                    bookRepository.save(book);
+              
+                    
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            );
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
 
 
     @PostConstruct
     public void start(){
-        System.out.println("Hey there this is Safal");
-        Author author = new Author();
-        author.setId("id");
-        author.setName("name");
-        author.setPersonalName("personalName");
-        authorRepository.save(author);
-
+        initAuthors();
+        //initWorks();
     }
 
 
