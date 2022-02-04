db8ff7ca7532c6a0b7c78d09e42dc6086c82f919
Safal Poudel <safalpoudel0223@gmail.com>
2022-02-03 19:59:18 -0600

    Book Page and Controller Added

--- main-app/src/main/java/com/mybooktracker/MyBookTracker.java
+++ main-app/src/main/java/com/mybooktracker/MyBookTracker.java
@@ -1,25 +1,38 @@
 package com.mybooktracker;
 
+import java.nio.file.Path;
+
+import com.mybooktracker.connection.DataStaxAstraProperties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class MyBookTracker {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyBookTracker.class, args);
 	}
 
-	@RequestMapping("/user")
-	public String user(@AuthenticationPrincipal OAuth2User principal) {
-		System.out.println(principal);
-		return principal.getAttribute("name");
-	}
+	// @RequestMapping("/user")
+	// public String user(@AuthenticationPrincipal OAuth2User principal) {
+	// 	System.out.println(principal);
+	// 	return principal.getAttribute("name");
+	// }
+
+	@Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
 	
 
 }
